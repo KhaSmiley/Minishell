cfe515c020a05c848e293046922efd7d929be2b9
Khalid <boulkrim.khalid@protonmail.com>
2024-02-28 18:40:14 +0100

    libft & Makefile

--- include/minishell.h
+++ include/minishell.h
@@ -6,44 +6,54 @@
 /*   By: kboulkri <kboulkri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 22:48:09 by kboulkri          #+#    #+#             */
-/*   Updated: 2024/02/27 02:28:22 by kboulkri         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:27:22 by kboulkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-typedef struct s_token t_token;
+typedef struct s_token	t_token;
 
 typedef struct s_token
 {
-    char *str;
-    int type;
-    struct s_token *next;
-} t_token;
+	char				*str;
+	int					type;
+	struct s_token		*next;
+}						t_token;
 
 enum
 {
-    GREATER,
-    DGREATER,
-    LESS,
-    DLESS,
-    PIPE,
-    SPACE,
-    WORD
+	GREATER,
+	DGREATER,
+	LESS,
+	DLESS,
+	PIPE,
+	SPACE,
+	WORD
 };
 
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
-# include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-# include <fcntl.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+# include <unistd.h>
 
-int ft_syntax(t_token *tok);
-int ft_syntax_pipe(t_token *tok);
-int ft_syntax_redir(t_token *tok);
-int ft_syntax_word(t_token *tok);
+/* lst_utils.c */
+
+void					print_list(t_token *lst);
+t_token					*ft_lstnew(char *content, int type);
+void					ft_stock(t_token **lst, t_token *new_link);
+t_token					*ft_lstlast(t_token *lst);
+
+/* syntax.c */
+
+int						ft_syntax(t_token *tok);
+int						ft_syntax_pipe(t_token *tok);
+int						ft_syntax_redir(t_token *tok);
+int						ft_syntax_word(t_token *tok);
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,128 +6,31 @@
 /*   By: kboulkri <kboulkri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 22:49:36 by kboulkri          #+#    #+#             */
-/*   Updated: 2024/02/27 03:08:27 by kboulkri         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:29:49 by kboulkri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// char *ft_stock_token(char *str)
-// {
-//     int i;
-
-//     i = 0;
-//     if (str[i] == LESS)
-// }
-
-// int ft_count_new(char *str)
-// {
-//     int i;
-//     int count;
-
-//     i = 0;
-//     count = 0;
-//     while(str[i])
-//     {
-//         if (str[i] == '<' && str[i + 1] == '<')
-//         {
-//             count+= 3;
-//             i++;
-//         }    
-//         else if (str[i] == '>' && str[i + 1] == '>')
-//         {
-//             count+= 3;
-//             i++;
-//         }
-//         else if (str[i] == '<' || str[i] == '>' || str[i] == '|')
-//             count += 2;
-//         i++;
-//         count++;
-//     }
-//     return (count);
-// }
-
-// char     *ft_tokenizer(char *token)
-// {
-//     if (token == "<")
-//         return ("LESS");
-//     else if (token == ">")
-//         return ("GREATER");
-//     else if (token == "<<")
-//         return ("DLESS");
-//     else if (token == ">>")
-//         return ("DGREATER");
-//     else if (token == "|")
-//         return ("PIPE");
-//     else if (token == " ")
-//         return ("SPACE");
-//     else
-//         return ("WORD");
-// } 
-
-t_token *ft_lstlast(t_token *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next)
-		lst = lst->next;
-	return (lst);
-}
-
-void	ft_stock(t_token **lst, t_token *new_link)
-{
-	if (!lst)
-		return ;
-	if (!*lst)
-		*lst = new_link;
-	else
-		(ft_lstlast(*lst))->next = new_link;
-}
-
-t_token	*ft_lstnew(char *content, int type)
-{
-	t_token	*new;
-
-	new = malloc(sizeof(*new));
-	if (!new)
-		return (NULL);
-	new->str = content;
-    new->type = type;
-	new->next = NULL;
-	return (new);
-}
-
 int     ft_tokenizer(char *token)
 {
-    if (strcmp(token, "<") == 0)
+    if (ft_strcmp(token, "<") == 0)
         return (LESS);
-    else if (strcmp(token, ">") == 0)
+    else if (ft_strcmp(token, ">") == 0)
         return (GREATER);
-    else if (strcmp(token, "<<") == 0)
+    else if (ft_strcmp(token, "<<") == 0)
         return (DLESS);
-    else if (strcmp(token, ">>") == 0)
+    else if (ft_strcmp(token, ">>") == 0)
         return (DGREATER);
-    else if (strcmp(token, "|") == 0)
+    else if (ft_strcmp(token, "|") == 0)
         return (PIPE);
-    else if (strcmp(token, " ") == 0)
+    else if (ft_strcmp(token, " ") == 0)
         return (SPACE);
     else
         return (WORD);
 } 
 
-void print_list(t_token *lst)
-{
-    if (!lst)
-        return ;
-    while(lst)
-    {
-        printf("WORD : %s TYPE : %d\n", lst->str, lst->type);
-        lst = lst->next;
-    }
-    return ;
-}
-
-int taille_mot(char *str, int i)
+int word_size(char *str, int i)
 {
     int j;
 
@@ -140,14 +43,14 @@ int taille_mot(char *str, int i)
     return (j);
 }   
 
-int    alloc_token(t_token *tok, char *longchev, char *str, int i)
+int    alloc_token(t_token **tok, char *longchev, char *str, int i)
 {
     if (str[i + 1] == longchev[0])
     {
-        ft_stock(&tok, ft_lstnew(longchev, ft_tokenizer(longchev)));
+        ft_stock(tok, ft_lstnew(longchev, ft_tokenizer(longchev)));
         return (1);
     }
-    ft_stock(&tok, ft_lstnew(&longchev[1], ft_tokenizer(&longchev[1])));  
+    ft_stock(tok, ft_lstnew(&longchev[1], ft_tokenizer(&longchev[1])));  
     return (0);
 }
 
@@ -160,14 +63,13 @@ void    find_token(char *str)
     t_token *tok;
     
     tok = NULL;
-    // word = malloc(sizeof(char) * 100);
     i = 0;
     while(str[i])
     {
         if (str[i] == '<')
-            i += alloc_token(tok, "<<", str, i);
+            i += alloc_token(&tok, "<<", str, i);
         else if (str[i] == '>')
-            i += alloc_token(tok, ">>", str, i);
+            i += alloc_token(&tok, ">>", str, i);
         else if (str[i] == '|')
             ft_stock(&tok, ft_lstnew("|", ft_tokenizer("|")));
         else if (str[i] == ' ')
@@ -175,8 +77,8 @@ void    find_token(char *str)
         else
         {
             j = 0;
-            word = malloc(taille_mot(str, i) + 1);
-            while (strchr(" \t><|\0", str[i]) == 0) // ft_
+            word = malloc(word_size(str, i) + 1);
+            while (ft_strchr(" \t><|\0", str[i]) == 0) // ft_
                 word[j++] = str[i++];
             word[j] = '\0';
             ft_stock(&tok, ft_lstnew(word, ft_tokenizer(word)));
@@ -186,21 +88,14 @@ void    find_token(char *str)
     }
     print_list(tok);
     ft_syntax(tok);
-    // while(tok)
-    // {
-    //     t_token *tmp;
-    //     tmp = tok;
-    //     printf("WORD : %s TYPE : %d\n", tok->str, tok->type);
-    //     tok = tok->next;
-    //     free(tmp);
-    // }
     return ;
 }   
 
 int main(int argc, char **argv, char **envp)
 {
-    t_token lst;
-
+    (void)argc;
+    (void)argv;
+    (void)envp;
     while (1)
     {
         char *input = readline("> ");
